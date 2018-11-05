3583859194f56ffc38125dd61a3ccc834df71f57
Robin Kim <rkim1214@gmail.com>
2018-11-05 22:31:37 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -44,7 +44,7 @@ def checkout(cart, coupons)
  
   total = 0.0
   newest_cart.each do |item, attributes|
-    total = total + (attributes[:price] * attributes[:count])
+    total += (attributes[:price] * attributes[:count])
   end 
   if total > 100.0
     total -= (total * 0.1)
