135bbeb7b6b268700f3fc9b7139f0e439a7af6fa
Robin Kim <rkim1214@gmail.com>
2018-11-05 21:54:48 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -38,7 +38,7 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  consolidate_cart([cart])
+  consolidate_cart(cart)
   apply_coupons(cart: [], coupons: [])
   apply_clearance(cart: [])
 end
